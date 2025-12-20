7c620863ce45862da8c94cccb13247ebd297a476
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-12-20 22:22:02 +0530

    adding changes to deploy on vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,12 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 
-import todoRoutes from './routes/todoRoutes.js';
-app.use('/api/todos', todoRoutes);
+// import todoRoutes from './routes/todoRoutes.js';
+// app.use('/api/todos', todoRoutes);
 
-import geminiRoutes from './routes/geminiRoutes.js';
-app.use('/api/gemini', geminiRoutes);
+// import geminiRoutes from './routes/geminiRoutes.js';
+// app.use('/api/gemini', geminiRoutes);
 
 export { app };
